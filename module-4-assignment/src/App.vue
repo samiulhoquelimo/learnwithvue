3d35936d74463533d3ef2430eaeee86bdc6eb031
Samiul Hoque Limo <samiul.limo@gmail.com>
2023-07-23 15:02:57 +0600

    module 4 assignment

--- module-4-assignment/src/App.vue
+++ module-4-assignment/src/App.vue
@@ -1,30 +1,84 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {ref} from 'vue'
+import {images} from "./data.js";
+
+const active = ref(0)
+const isLoading = ref(true)
+const imageFilter = '-webkit-filter: grayscale(100%);filter: grayscale(100%);'
+
+const onImageLoad = () => {
+  isLoading.value = false
+}
+const next = () => {
+  isLoading.value = true;
+  return (images.length) - 1 === active ? active.value = 0 : active.value++
+}
+const back = () => {
+  isLoading.value = true;
+  return 0 === active ? active.value = (images.length) - 1 : active.value--;
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h4>Vue.js 3 Image Carousel</h4>
+
+  <div class="carousel" data-ride="carousel">
+
+    <ol class="carousel-indicators">
+      <li v-for="(image, index) in images" :key="image.id" :class="index === active?'active':''"
+          @click="active = index"/>
+    </ol>
+
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img :src="images[active].download_url" alt="_" class="d-block w-100 img img-responsive"
+             height="700" @load="onImageLoad">
+        <div class="carousel-caption d-none d-md-block">
+          <div v-show="isLoading" class="loader"></div>
+          <h5>{{ images[active].author }}</h5>
+          <a v-show="!isLoading" :href="images[active].url" target="_blank">Download</a>
+        </div>
+      </div>
+      <a class="carousel-control-prev"
+         data-slide="prev" href="#" role="button" @click.prevent="back">
+        <span aria-hidden="true" class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next"
+         data-slide="next" href="#" role="button" @click.prevent="next">
+        <span aria-hidden="true" class="carousel-control-next-icon"></span>
+      </a>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <br>
+  <ol>
+    <img v-for="(image, index) in images" :key="image.id" :src="image.download_url"
+         :style="active !== index?imageFilter:''"
+         alt="" class="mx-1" height="40" style="cursor: pointer;" @click="active=index">
+  </ol>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+.loader {
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #3498db;
+  border-radius: 50%;
+  margin: auto;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
