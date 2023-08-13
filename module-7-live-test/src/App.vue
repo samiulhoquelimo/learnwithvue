ed535807a31b9998f6c79e7d6f389d5d6105da47
Samiul Hoque Limo <samiul.limo@gmail.com>
2023-08-13 23:28:02 +0600

    module 7 live test

---
+++ module-7-live-test/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DataFetchingComponent from "./components/DataFetchingComponent.vue";
+</script>
+
+<template>
+  <DataFetchingComponent msg="Vite + Vue"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ module-7-live-test/src/components/DataFetchingComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+import {onBeforeUnmount, onMounted, ref} from "vue";
+
+const user = ref(null);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://dummyjson.com/users/1');
+    user.value = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+})
+
+onBeforeUnmount(() => {
+  user.value = null
+})
+
+</script>
+
+<template>
+  <div>
+    <h2>Fetches data from an API</h2>
+    <p>{{ user }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
