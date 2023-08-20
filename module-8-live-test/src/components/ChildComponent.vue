75fcb881c600c83b90a68134f4a625a0772bba26
Samiul Hoque Limo <samiul.limo@gmail.com>
2023-08-20 23:15:20 +0600

    module 8 live test

---
+++ module-8-live-test/src/components/ChildComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['userName'])
+</script>
+
+<template>
+  <h2>Child Component</h2>
+  <p>Here we get user name {{ userName }} from defineProps</p>
+  <p>{{ userName }}</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ module-8-live-test/src/components/ParentComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ChildComponent from "./ChildComponent.vue";
+
+const userName = 'Md. Samiul Hoque'
+</script>
+
+<template>
+  <h2>Parent Component</h2>
+  <p>Here we pass user name {{ userName }} to the child component</p>
+  <hr>
+  <ChildComponent :user-name="userName"/>
+</template>
+
+<style scoped>
+
+</style>
