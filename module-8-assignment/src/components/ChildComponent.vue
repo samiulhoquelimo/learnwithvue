d6777fd53c386eaaa8a9639301d81cd54377a8b1
Samiul Hoque Limo <samiul.limo@gmail.com>
2023-08-20 10:53:53 +0600

    module 8 assignment

---
+++ module-8-assignment/src/components/ChildComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(['message'])
+const emit = defineEmits(['messageEmitted'])
+const emitMessageClick = () => {
+  emit('messageEmitted', 'This is the message send from child')
+}
+</script>
+
+<template>
+  <h2 class="text-2xl my-10">Message show in Child</h2>
+  <p class="my-5">{{ message }}</p>
+  <button @click="emitMessageClick"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Click from child
+  </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ module-8-assignment/src/components/ParentComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+import ChildComponent from "./ChildComponent.vue";
+import {ref} from "vue";
+
+const message = ref('No message has been set yet')
+
+const messageEmitted = (msg) => {
+  message.value = msg
+}
+
+const messageEmittedFromParent = () => {
+  message.value = "This is the message send from parent"
+}
+
+</script>
+
+<template>
+  <h2 class="text-2xl my-10">Message show in Parent</h2>
+  <p class="my-5">{{ message }}</p>
+  <p>
+    <button @click="messageEmittedFromParent"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Click from parent
+    </button>
+  </p>
+  <hr>
+  <p>
+    <ChildComponent :message="message" @messageEmitted="messageEmitted"/>
+  </p>
+
+</template>
+
+<style scoped>
+
+</style>
