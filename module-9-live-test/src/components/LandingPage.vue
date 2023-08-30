ba572dba25683cd3fd3083a6a65bb783c0631270
Samiul Hoque Limo <samiul.limo@gmail.com>
2023-08-30 23:31:05 +0600

    module 9 live test

---
+++ module-9-live-test/src/components/LandingPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['username'])
+</script>
+
+<template>
+  <div>
+    <h1>Welcome, {{ username }}!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ module-9-live-test/src/components/LoginPage.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from 'vue';
+import LandingPage from "./LandingPage.vue";
+
+const username = ref('');
+const password = ref('');
+const isLoggedIn = ref(false);
+
+const login = () => {
+  if (username.value && password.value) {
+    isLoggedIn.value = true
+  }
+};
+
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="login" v-show="!isLoggedIn">
+      <label for="username">Username/Email:</label>
+      <input v-model="username" type="text" id="username" required/>
+      <br/>
+      <label for="password">Password:</label>
+      <input v-model="password" type="password" id="password" required/>
+      <br/>
+      <button type="submit">Login</button>
+    </form>
+
+    <LandingPage :username="username" v-show="isLoggedIn"/>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
